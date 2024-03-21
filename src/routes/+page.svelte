5640ab49860cad5d5187b633f2babbe7a73ab5dd
Daniel Llamas <daniel@soyllamas.com>
2024-03-21 15:05:57 -0600

    feat: add basic title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="max-w-[960px] mx-auto pb-[80px] pt-[40px]">
+    <h1 class="text-slate-950 text-[32px] font-bold">Simple Boards</h1>
+    <h2 class="text-slate-600">Kanban boards for everyday use.</h2>
+</div>
