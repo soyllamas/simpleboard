97e46ae289612e2646e5eb04e105c1ca8c45c9e5
Daniel Llamas <daniel@soyllamas.com>
2024-03-23 12:50:20 -0600

    chore: add playground

---
+++ src/routes/playground/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    let times = $state(0)
+
+    function onClick() {
+        times++
+    }
+</script>
+
+<div class="h-dvh grid grid-cols-1 gap-4">
+    <p class="self-end justify-self-center">Pressed {times} times</p>
+    <button class="self-start justify-self-center px-4 bg-slate-950 rounded-lg min-h-12 min-w-6 border-[1px] border-slate-700 text-slate-50"
+            onclick={() => onClick()}>
+        Click Me!
+    </button>
+</div>
