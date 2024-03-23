475a40ad5a6b80dc38658960337b4f33241f6c99
Daniel Llamas <daniel@soyllamas.com>
2024-03-23 13:08:25 -0600

    feat: poc for debounce using rx

--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -1,7 +1,20 @@
-<script>
-    let times = $state(0)
+<script lang="ts">
+    import {debounceTime, Subject, Subscription} from 'rxjs';
+    import {onDestroy, onMount} from "svelte";
 
-    function onClick() {
+    let times = $state(0)
+    let subscription: Subscription
+    const observable = new Subject<number>();
+
+    onMount(() => {
+        const debounceTimeInMills = debounceTime(1000)
+        const debouncedObservable = observable.pipe(debounceTimeInMills)
+        subscription = debouncedObservable.subscribe(_onClick);
+    })
+
+    onDestroy(() => subscription?.unsubscribe())
+
+    function _onClick() {
         times++
     }
 </script>
@@ -9,7 +22,7 @@
 <div class="h-dvh grid grid-cols-1 gap-4">
     <p class="self-end justify-self-center">Pressed {times} times</p>
     <button class="self-start justify-self-center px-4 bg-slate-950 rounded-lg min-h-12 min-w-6 border-[1px] border-slate-700 text-slate-50"
-            onclick={() => onClick()}>
+            onclick={() => observable.next(times)}>
         Click Me!
     </button>
 </div>
