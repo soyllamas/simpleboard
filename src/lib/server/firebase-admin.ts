0e3e8901167730063c0613c453a8d972f3be999e
Daniel Llamas <daniel@soyllamas.com>
2024-03-24 20:06:42 -0600

    feat: migrate service account to an environment variable

--- src/lib/server/firebase-admin.ts
+++ src/lib/server/firebase-admin.ts
@@ -1,6 +1,8 @@
 import {initializeApp, cert, getApps, getApp} from 'firebase-admin/app';
-import serviceAccount from './serviceAccount.json';
 import {getFirestore} from "firebase-admin/firestore";
+import {FIREBASE_SERVICE_ACCOUNT} from "$env/static/private";
+
+const serviceAccount = JSON.parse(atob(FIREBASE_SERVICE_ACCOUNT))
 
 // @ts-ignore
 !getApps().length ? initializeApp({credential: cert(serviceAccount)}) : getApp();
