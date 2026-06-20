72ed18896010d827444241c6ffb2c4b21917317b
Daniel Llamas <daniel@soyllamas.com>
2026-06-19 23:03:50 -0600

    fix: avoid unused analytics initialization

--- src/lib/client/firebase.ts
+++ src/lib/client/firebase.ts
@@ -1,12 +1,10 @@
 const { initializeApp, getApps, getApp } = await import("@firebase/app");
 const { getFirestore } = await import("@firebase/firestore");
-const { getAnalytics } = await import("@firebase/analytics");
 
 import { PUBLIC_FIREBASE_CONFIG } from "$env/static/public";
 
 const config = JSON.parse(atob(PUBLIC_FIREBASE_CONFIG));
 const app = getApps().length === 0 ? initializeApp(config) : getApp();
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
-export { db, analytics };
+export { db };
